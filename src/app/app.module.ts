dcecad3e99b82440ed2c26e63ddaae4c6793df08
Wataru.Kasahara <kasahara1019@gmail.com>
2020-10-11 18:53:33 +0900

    yarn ng g c features/demo2/ui/demo2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DemoComponent } from './features/demo/ui/demo/demo.component';
+import { Demo2Component } from './features/demo2/ui/demo2/demo2.component';
 import { HomeComponent } from './features/home/ui/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, DemoComponent],
+  declarations: [AppComponent, HomeComponent, DemoComponent, Demo2Component],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/features/demo2/ui/demo2/demo2.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo2',
+  templateUrl: './demo2.component.html',
+  styleUrls: ['./demo2.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Demo2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
