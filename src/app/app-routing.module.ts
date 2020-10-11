c1d9313ab22f9415675952ea3cc409c24be3ac5e
Wataru.Kasahara <kasahara1019@gmail.com>
2020-10-11 19:03:38 +0900

    feat: ルーティングの整理

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DemoComponent } from './features/demo/ui/demo/demo.component';
+import { Demo2Component } from './features/demo2/ui/demo2/demo2.component';
+import { HomeComponent } from './features/home/ui/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'demo', component: DemoComponent },
+  { path: 'demo2', component: Demo2Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
