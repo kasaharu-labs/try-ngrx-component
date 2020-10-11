5a7cebc36f6c63149b7a72a96f41e4b63ef5d78e
Wataru.Kasahara <kasahara1019@gmail.com>
2020-10-11 18:53:15 +0900

    yarn ng g c features/demo/ui/demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DemoComponent } from './features/demo/ui/demo/demo.component';
 import { HomeComponent } from './features/home/ui/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, DemoComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
